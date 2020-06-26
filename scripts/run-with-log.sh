83dc8b29128e66737e864b85a620e586ae481555
Anthony Pengelly <anthony.pengelly@camelotls.com>
2020-06-26 12:17:43 +0100

    Remove unneeded vars from cli call

--- scripts/run-with-log.sh
+++ scripts/run-with-log.sh
@@ -5,5 +5,5 @@ mkdir -p log
 mkdir -p data
 DATE=$(date +'%Y-%m-%d')
 
-./bin/fpl-cli run $FPL_EMAIL $FPL_PASSWORD $FPL_TEAM_ID &> "log/$DATE.log"
+./bin/fpl-cli run &> "log/$DATE.log"
 cat "log/$DATE.log"
