63ebf77b4eca44c8c57e86f17ed92accd72e815d
Anthony Pengelly <anthony.pengelly@camelotls.com>
2020-06-28 13:41:50 +0100

    Add authentication for tests to test the interface with the API

---
+++ scripts/run-tests.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+cd ..
+
+if [ -z "$FPL_EMAIL" ]
+  then
+    echo "Please include the FPL_EMAIL variable"
+    exit 1
+fi
+
+if [ -z "$FPL_PASSWORD" ]
+  then
+    echo "Please include the FPL_PASSWORD variable"
+    exit 1
+fi
+
+
+# The Cookies returned are incompatible with node libraries, so we login here and set the response
+# as an env variable, allowing the JS to parse them
+export FPL_AUTH_HEADERS=$(curl -s https://users.premierleague.com/accounts/login/ \
+  -H 'content-type: application/x-www-form-urlencoded' \
+  --data-raw "login=$FPL_EMAIL&password=$FPL_PASSWORD&app=plfpl-web&redirect_uri=https%3A%2F%2Ffantasy.premierleague.com%2F" \
+  -i)
+
+./node_modules/.bin/jest ./lib/tests
--- src/tests/fetchers/fplFetcher.test.ts
+++ src/tests/fetchers/fplFetcher.test.ts
@@ -1,3 +1,46 @@
+import FplFetcher from "../../fetchers/fplFetcher";
+
+const fplFetcher = new FplFetcher();
+
+describe("fplFetcher", () => {
+  test("getOverview returns required fields", async () => {
+    const result = await fplFetcher.getOverview();
+
+    expect(result.elements.length).toBeGreaterThan(100);
+    expect(result.elements[0].id).toBeTruthy();
+    expect(result.elements[0].web_name).toBeTruthy();
+    expect(result.elements[0].now_cost).toBeTruthy();
+    expect(result.elements[0].element_type).toBeTruthy();
+    expect(result.elements[0].team).toBeTruthy();
+    expect(result.elements[0].code).toBeTruthy();
+    expect(result.elements[0].form).toBeTruthy();
+    expect(result.elements[0].points_per_game).toBeTruthy();
+    expect(result.elements[0].ict_index).toBeTruthy();
+    expect(result.elements[0].chance_of_playing_next_round).toBeTruthy();
+
+    expect(result.events.length).toBeGreaterThan(20);
+    expect(result.events[0].id).toBeTruthy();
+    expect(result.events[0].deadline_time).toBeTruthy();
+    expect(result.events[0].finished === true || result.events[0].finished === false).toBeTruthy();
+    expect(result.events[0].is_next === true || result.events[0].is_next === false).toBeTruthy();
+
+    expect(result.teams.length).toBe(20);
+    expect(result.teams[0].id).toBeTruthy();
+    expect(result.teams[0].short_name).toBeTruthy();
+    expect(result.teams[0].strength).toBeTruthy();
+    expect(result.teams[0].strength_attack_away).toBeTruthy();
+    expect(result.teams[0].strength_attack_home).toBeTruthy();
+    expect(result.teams[0].strength_defence_away).toBeTruthy();
+    expect(result.teams[0].strength_defence_home).toBeTruthy();
+    expect(result.teams[0].strength_overall_away).toBeTruthy();
+    expect(result.teams[0].strength_overall_home).toBeTruthy();
+
+    expect(result.element_types.length).toBe(4);
+    expect(result.element_types[0].id).toBeTruthy();
+    expect(result.element_types[0].singular_name).toBeTruthy();
+  });
+});
+
 // Overview returns correct shape
 
 // get player returns correct shape
