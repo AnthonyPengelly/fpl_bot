3ede178dfcd18d01a2cfa632d0f49e6807370c83
Anthony Pengelly <anthony.pengelly@camelotls.com>
2020-06-28 09:27:00 +0100

    Update scores to remove focus on the players team a little to avoid players that don't actually play getting good scores

--- src/config/scoreSettings.ts
+++ src/config/scoreSettings.ts
@@ -17,11 +17,11 @@ export interface ScoreSettings {
 
 const defenderSettings: ScoreSettings = {
   weights: {
-    form: { max: 8, weight: 15 },
-    pointsPerGame: { max: 6, weight: 10 },
+    form: { max: 8, weight: 23 },
+    pointsPerGame: { max: 6, weight: 15 },
     ictIndex: { max: 450, weight: 10 },
-    teamStrength: { max: 5, weight: 18, min: 2 },
-    teamStrengthForPosition: { max: 1350, weight: 20, min: 950 },
+    teamStrength: { max: 5, weight: 10, min: 2 },
+    teamStrengthForPosition: { max: 1350, weight: 15, min: 950 },
     opponentStrength: { max: 5 + 2, weight: 15, min: 2 }, // home advantage to max opp strength
     futureOpponentStrength: { max: 5 + 2, weight: 12, min: 2 },
     chanceOfPlaying: { max: 100, weight: 50 },
@@ -32,11 +32,11 @@ const defenderSettings: ScoreSettings = {
 
 const attackerSettings: ScoreSettings = {
   weights: {
-    form: { max: 8, weight: 22 },
+    form: { max: 8, weight: 30 },
     pointsPerGame: { max: 6, weight: 15 },
-    ictIndex: { max: 450, weight: 20 },
-    teamStrength: { max: 5, weight: 10, min: 2 },
-    teamStrengthForPosition: { max: 1350, weight: 10, min: 950 },
+    ictIndex: { max: 450, weight: 22 },
+    teamStrength: { max: 5, weight: 5, min: 2 },
+    teamStrengthForPosition: { max: 1350, weight: 5, min: 950 },
     opponentStrength: { max: 5 + 1, weight: 13, min: 2 }, // home advantage to max opp strength
     futureOpponentStrength: { max: 5 + 1, weight: 10, min: 2 },
     chanceOfPlaying: { max: 100, weight: 50 },
